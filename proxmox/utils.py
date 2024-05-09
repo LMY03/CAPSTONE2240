8c3553e0a77e8cc7210aa63d8b6a599cb68849de
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-05-10 07:17:39 +0800

    proxmox api functions

--- proxmox/utils.py
+++ proxmox/utils.py
@@ -1,4 +1,6 @@
 import requests
+from django.conf import settings
+from django.core.cache import cache
 
 # Parameters
 PROXMOX_HOST = 'https://192.168.254.112:8006'
@@ -23,25 +25,93 @@ def get_authenticated_session():
     })
     return session
 
-# create VM
+# create VM POST
 def create_vm(node, vmid, config):
     session = get_authenticated_session()
     url = f"{PROXMOX_HOST}/api2/json/nodes/{node}/qemu/{vmid}/config"
+    config = {
+    }
     response = session.post(url, data=config)
     return response.json()
 
-# clone VM
+# clone VM POST
 def clone_vm(node, vmid, newid):
     session = get_authenticated_session()
     node = "pve"
     url = f"{PROXMOX_HOST}/api2/json/nodes/{node}/qemu/{vmid}/clone"
-    data = {
+    config = {
         'newid': newid,
         'full': 1,
         # 'target': ''
         # 'storage': 'local-lvm',
     }
-    response = session.post(url, data=data)
-    if response.status_code != 200:
-        return {'error': 'Failed to clone VM', 'status_code': response.status_code, 'details': response.text}
+    response = session.post(url, data=config)
     return response.json()
+
+
+# delete VM DELETE
+def delete_vm(node, vmid):
+    session = get_authenticated_session()
+    node = "pve"
+    url = f"{PROXMOX_HOST}/api2/json/nodes/{node}/qemu/{vmid}"
+    response = session.delete(url)
+    return response.json()
+
+# start VM POST
+def start_vm(node, vmid):
+    session = get_authenticated_session()
+    node = "pve"
+    url = f"{PROXMOX_HOST}/api2/json/nodes/{node}/qemu/{vmid}/status/start"
+    response = session.post(url)
+    return response.json()
+
+# shutdown VM POST
+def shutdown_vm(node, vmid):
+    session = get_authenticated_session()
+    node = "pve"
+    url = f"{PROXMOX_HOST}/api2/json/nodes/{node}/qemu/{vmid}/status/shutdown"
+    response = session.post(url)
+    return response.json()
+
+# shutdown VM PUT
+def config_vm(node, vmid, cpu_cores, memory_mb, disk_mb):
+    session = get_authenticated_session()
+    node = "pve"
+    url = f"{PROXMOX_HOST}/api2/json/nodes/{node}/qemu/{vmid}/status/shutdown"
+    config = {
+        'cores': cpu_cores,
+        'memory': memory_mb,
+        'virtio0': disk_mb # example diskmb = 'local-lvm:vm-100-disk-1,size=32G' # TODO angline check 
+    }
+    response = session.put(url)
+    return response.json()
+
+# get vm ip 
+def get_vm_ip(node, vmid):
+    url = f"{PROXMOX_HOST}/api2/json/nodes/{node}/qemu/{vmid}/agent/network-get-interfaces"
+    # headers = {
+    #     'Authorization': 'Bearer YOUR_ACCESS_TOKEN',
+    #     'CSRFPreventionToken': 'YOUR_CSRF_TOKEN'
+    # }
+    # response = requests.get(url, headers=headers, verify=False)  # Verify should ideally be True in production
+    response = requests.get(url, verify=False)
+    data = response.json()
+
+    # Parsing the response to extract IP addresses
+    if 'result' in data:
+        interfaces = data['result']
+        for interface in interfaces:
+            if 'ip-addresses' in interface:
+                for ip in interface['ip-addresses']:
+                    if ip['ip-address-type'] == 'ipv4':  # or ipv6 if you prefer
+                        return ip['ip-address']
+    return "No IP address found"
+
+
+
+
+# Create Template - 1. upload ISO, 2. set existing vm to template
+
+
+
+
--- proxmox/views.py
+++ proxmox/views.py
@@ -1,33 +1,3 @@
-from django.http import JsonResponse
-from . import utils
+from django.shortcuts import render
 
 # Create your views here.
-def create_vm(request):
-    if request.method == 'POST':
-        node = request.POST.get('node')
-        vmid = request.POST.get('vmid')
-        config = {
-            'memory': request.POST.get('memory'),
-            'cores': request.POST.get('cores'),
-            'net0': 'virtio,bridge=vmbr0',
-            # Add other configurations as needed
-        }
-        result = utils.create_vm(node, vmid, config)
-        return JsonResponse({'status': 'success', 'data': result})
-    else:
-        return JsonResponse({'status': 'error', 'message': 'Invalid method'}, status=405)
-    
-def clone_vm(request):
-    if request.method == 'POST':
-        node = request.POST.get('node')
-        vmid = request.POST.get('vmid')
-        newid = request.POST.get('newid')
-        config = {
-            'name': request.POST.get('name', ''),  # Default empty if not provided
-            'storage': request.POST.get('storage', '')  # Default empty if not provided
-            # Include other options as needed
-        }
-        result = utils.clone_vm(node, vmid, newid, config)
-        return JsonResponse({'status': 'success', 'data': result})
-    else:
-        return JsonResponse({'status': 'error', 'message': 'Invalid method'}, status=405)
