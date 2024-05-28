9afe3892efbf1e9888604fa88cc335810f253b8a
patrick-ong <patrick_bennett_ong@dlsu.edu.ph>
2024-05-28 17:50:20 +0800

    Added comment, login, and fixed the OS list foreign key

--- CAPSTONE2240/urls.py
+++ CAPSTONE2240/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from ticketing import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('redirect/', views.redirect_based_on_user_type, name= 'redirect_based_on_user_type'),
     path('ticketing/', include("ticketing.urls")),
+    path('', auth_views.LoginView.as_view(template_name='ticketing/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('social-auth/', include('social_django.urls', namespace='social')),
     path('users/', include("users.urls")),
     path('admin/', admin.site.urls),
 ]
