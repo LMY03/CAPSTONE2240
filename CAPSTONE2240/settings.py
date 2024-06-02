d882a443225f7a2b6c47e83cd83c0f6f01c4c781
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-06-02 16:36:59 +0800

    containerize update

--- CAPSTONE2240/settings.py
+++ CAPSTONE2240/settings.py
@@ -92,6 +92,7 @@ DATABASES = {
         'NAME': 'cap2240db',
         'USER': 'cap-2240',
         'PASSWORD': 'CAP_2240',
+        # 'HOST': 'localhost',
         'HOST': 'mysql',
         'PORT': 3306,
     }
@@ -134,11 +135,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
