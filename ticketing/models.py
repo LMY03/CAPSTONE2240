8874767e878b74a6cc15c934f89e48544af314d4
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-06-11 15:17:20 +0800

    Update models.py

--- ticketing/models.py
+++ ticketing/models.py
@@ -3,7 +3,6 @@ from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.postgres.fields import JSONField
 from django.utils import timezone
 from datetime import date, timedelta
 
