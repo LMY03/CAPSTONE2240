92d1ce34d14100d4090d6eabb9d73389f408d2ea
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-06-24 23:42:50 +0800

    Update init.sql

--- init.sql
+++ init.sql
@@ -7,8 +7,8 @@ INSERT INTO cap2240db.ticketing_vmtemplates (vm_id, vm_name, storage) VALUES
 
 INSERT INTO cap2240db.auth_user (password, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined) VALUES
 ("pbkdf2_sha256$720000$5gL9pa3JAHZYMUbNgW3qqL$udv7QLPHZ/Fv5ijQQMvklg06MOZvEkkbGY2LJ17dIyM=", "1", "admin", "", "", "", "1", "1", NOW()),
-("pbkdf2_sha256$720000$5gL9pa3JAHZYMUbNgW3qqL$udv7QLPHZ/Fv5ijQQMvklg06MOZvEkkbGY2LJ17dIyM=", "1", "faculty", "", "", "", "1", "1", NOW());
+("pbkdf2_sha256$720000$5gL9pa3JAHZYMUbNgW3qqL$udv7QLPHZ/Fv5ijQQMvklg06MOZvEkkbGY2LJ17dIyM=", "0", "faculty", "", "", "", "1", "1", NOW());
 
-INSERT INTO cap2240.ticketing_userprofile (user_type, user_id) VALUES
+INSERT INTO cap2240db.ticketing_userprofile (user_type, user_id) VALUES
 ("admin", 1),
 ("faculty", 2);
