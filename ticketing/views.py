c5c00debd426148651b0c0ad317e79e32e4f3428
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-06-24 15:41:27 +0800

    Update views.py

--- ticketing/views.py
+++ ticketing/views.py
@@ -144,7 +144,8 @@ def new_form_submit(request):
     if request.method == "POST":
         print ("new-form-submit-post")
         # get data
-        requester = get_object_or_404(User, username=request.user)
+        # requester = get_object_or_404(User, username=request.user)
+        requester = "1"
         data = request.POST
         template_id = data.get("template_id")
         cores = data.get("cores")
