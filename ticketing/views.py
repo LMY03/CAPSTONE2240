251f944e494f71f4ecc1b0eb4e90fcd1b8167634
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-06-27 07:33:54 +0800

    Update views.py

--- ticketing/views.py
+++ ticketing/views.py
@@ -293,7 +293,7 @@ def vm_provision(id):
     for request_use_case in request_use_cases:
         for i in range(request_use_case['vm_count']):
             classnames.append(f"{request_use_case['request_use_case'].replace('_', '-')}-Group-{i + 1}")
-            total_no_of_vm += int(request_use_case['vm_count'])
+        total_no_of_vm += int(request_use_case['vm_count'])
 
     cpu_cores = int(request_entry.cores)
     ram = int(request_entry.ram)
