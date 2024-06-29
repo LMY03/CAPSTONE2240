98e9ef46bf8caca1d8a60d8b32f0d1a473c0e80c
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-06-29 18:10:39 +0800

    Update views.py

--- guacamole/views.py
+++ guacamole/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.http import JsonResponse
 
 # from django.contrib.auth.models import User
@@ -7,6 +7,7 @@ from . import guacamole
 from proxmox import proxmox
 
 from guacamole.models import GuacamoleUser, GuacamoleConnection
+from proxmox.models import VirtualMachines
 
 # Create your views here.
 
@@ -23,7 +24,7 @@ def launch_vm(request):
     print("launch vm ---------------------------")
     if request.method == "POST":
 
-        node = "pve"
+        node = get_object_or_404(VirtualMachines, vm_id=vm_id).node
 
         data = request.POST
         vm_id = data.get("vm_id")
@@ -34,19 +35,16 @@ def launch_vm(request):
         guacamole_user = GuacamoleUser.objects.get(system_user=request.user)
         guacamole_username = guacamole_user.username
         guacamole_password = guacamole_user.password
-        # guacamole_password = request.user.password
-        print("guacamole_username: " + guacamole_username)
-        print("guacamole_password: " + guacamole_password)
         # guacamole_username = "guacadmin"
         # guacamole_password = "guacadmin"
-
-        # if proxmox.get_vm_status(node, vm_id) == "stopped" : proxmox.start_vm(node, vm_id)
-        # hostname = proxmox.wait_and_get_ip(node, vm_id)
-        # connection_details = guacamole.get_connection_parameter_details(connection_id)
-        # if hostname != connection_details['hostname'] : guacamole.update_connection(connection_id, hostname)
+        connection_id = get_object_or_404(GuacamoleConnection, vm_id=vm_id).connection_id
+        if proxmox.get_vm_status(node, vm_id) == "stopped" : proxmox.start_vm(node, vm_id)
+        hostname = proxmox.wait_and_get_ip(node, vm_id)
+        connection_details = guacamole.get_connection_parameter_details(connection_id)
+        if hostname != connection_details['hostname'] : guacamole.update_connection(connection_id, hostname)
         
         # redirect to new tab
-        url =  guacamole.get_connection_url(GuacamoleConnection.objects.get(vm_id=vm_id).connection_id, guacamole_username, guacamole_password)
+        url =  guacamole.get_connection_url(connection_id, guacamole_username, guacamole_password)
         
         return JsonResponse({"redirect_url": url})
     
