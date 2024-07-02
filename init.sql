f1b59598b2d30f51f17b00d0d38a2c339d10b097
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-07-02 18:08:54 +0800

    update

--- init.sql
+++ init.sql
@@ -33,10 +33,10 @@ DELIMITER ;
 
 -- cap2240db
 
-INSERT INTO cap2240db.ticketing_vmtemplates (vm_id, vm_name, cores, ram, storage) VALUES
-("3000", "Ubuntu-Desktop-22-RDP", 1, 1024, 20),
-("4000", "Ubuntu-Desktop-24-RDP", 1, 1024, 20),
-("5000", "Ubuntu-LXC-23", 1, 1024, 10);
+INSERT INTO cap2240db.ticketing_vmtemplates (vm_id, vm_name, cores, ram, storage, node, is_lxc) VALUES
+("3000", "Ubuntu-Desktop-22-RDP", 1, 1024, 20, "pve", 0),
+("4000", "Ubuntu-Desktop-24-RDP", 1, 1024, 20, "pve", 0),
+("5000", "Ubuntu-LXC-23", 1, 1024, 10, "pve", 1);
 
 INSERT INTO cap2240db.auth_user (password, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined) VALUES
 ("pbkdf2_sha256$720000$5gL9pa3JAHZYMUbNgW3qqL$udv7QLPHZ/Fv5ijQQMvklg06MOZvEkkbGY2LJ17dIyM=", "1", "admin", "admin", "chan", "", "1", "1", NOW());
