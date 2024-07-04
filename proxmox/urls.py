c6bafd15e98d41dadc1de1f7bad7e2b62b7281e7
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-07-04 23:36:52 +0800

    Update urls.py

--- proxmox/urls.py
+++ proxmox/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', views.vm_list, name='index'),
     path('<int:vm_id>/details', views.vm_details, name='vm_details'),
     path('shutdown_vm/<int:vm_id>', views.shutdown_vm, name='shutdown_vm'),
-    path('generate_vm_ids/<int:vm_id>', views.generate_vm_id_view, name='generate_vm_ids'),
+    path('generate_vm_ids/<int:no_of_vm>', views.generate_vm_id_view, name='generate_vm_ids'),
     
     # path("form", views.renders, name="form"),
     # path("clone_vm", views.clone_vm, name="clone_vm"),
