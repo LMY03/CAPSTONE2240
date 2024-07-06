064ba74ee9e062ce16632db3c26f67138b88a071
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-07-07 03:22:15 +0800

    Update views.py

--- autotool/views.py
+++ autotool/views.py
@@ -20,7 +20,7 @@ def run(request):
         proxmox.wait_for_task(node, proxmox.clone_vm(node, 3000, new_vm_id, 'Tester'))
         proxmox.start_vm(node, new_vm_id)
         ip_add = proxmox.wait_and_get_ip(node, new_vm_id)
-        response = ansible.resize_vm_disk(node, new_vm_id, ip_add)
+        response = ansible.resize_vm_disk(node, new_vm_id, "50G", ip_add)
 
         return render(request, "data.html", { "data" : response })
 
