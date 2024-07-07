857d189b033945f04216b5040b4a47d483457d86
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-07-07 11:26:04 +0800

    Update models.py

--- proxmox/models.py
+++ proxmox/models.py
@@ -15,7 +15,7 @@ class VirtualMachines(models.Model):
     request = models.ForeignKey('ticketing.RequestEntry', on_delete=models.CASCADE)
     node = models.CharField(max_length=45)
     is_lxc = models.BooleanField(default=False)
-    # vm_password = models.CharField(max_length=45)
+    vm_password = models.CharField(max_length=45, default=config('DEFAULT_VM_PASSWORD'))
 
     class Status(models.TextChoices): 
         ACTIVE = 'ACTIVE'
@@ -27,16 +27,3 @@ class VirtualMachines(models.Model):
         choices=Status.choices,
         default=Status.ACTIVE
     )
-
-    def get_vm_userpass(self):
-        vm_user = VMUser.objects.get(vm=self)
-        try:
-            vm_user = VMUser.objects.get(vm=self)
-            return {'vm_user': vm_user.username, 'vm_pass': vm_user.password}
-        except VMUser.DoesNotExist:
-            return {'vm_user': config('DEFAULT_VM_USERNAME'), 'vm_pass': config('DEFAULT_VM_PASSWORD')}
-
-class VMUser(models.Model):
-    vm = models.ForeignKey(VirtualMachines, on_delete=models.CASCADE)
-    username = models.CharField(max_length=45)
-    password = models.CharField(max_length=45)
