c24d59335c1f66f2a1657cf1d7d562dd586bb9d1
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-07-09 14:15:06 +0800

    Update settings.py

--- CAPSTONE2240/settings.py
+++ CAPSTONE2240/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "ticketing.apps.TicketingConfig",
     "guacamole.apps.GuacamoleConfig",
     "opnsense.apps.OpnsenseConfig",
+    "pfsense.apps.PfsenseConfig",
     "proxmox.apps.ProxmoxConfig",
     'django.contrib.admin',
     'django.contrib.auth',
