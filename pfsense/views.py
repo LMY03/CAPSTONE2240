dffc351b17656299cc2d5322679e2dcfb4cbd1b6
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-07-10 13:05:52 +0800

    Update views.py

--- pfsense/views.py
+++ pfsense/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import get_list_or_404
-import redis, os
+# import redis, os
 
 from . import pfsense
 
 from ticketing.models import RequestEntry, PortRules
 
-redis_host = os.getenv('REDIS_HOST', 'redis')
-redis_client = redis.StrictRedis(host=redis_host, port=6379, db=0)
+# redis_host = os.getenv('REDIS_HOST', 'redis')
+# redis_client = redis.StrictRedis(host=redis_host, port=6379, db=0)
 
 # Create your views here.
 
@@ -31,27 +31,29 @@ def generate_dest_ports():
 
 def add_port_forward_rules(protocols, ip_adds, local_ports, descrs):
     dest_ports = generate_dest_ports()
-    lock = redis_client.lock('pfsense_lock', timeout=60)
-    with lock:
-        for protocol, destination_port, ip_add, local_port, descr in protocols, dest_ports, ip_adds, local_ports, descrs:
-            pfsense.add_firewall_rule(protocol, destination_port, ip_add, descr)
-            pfsense.add_port_forward_rule(protocol, destination_port, ip_add, local_port, descr)
-    pfsense.apply_changes()
+    # lock = redis_client.lock('pfsense_lock', timeout=60)
+    # with lock:
+    #     for protocol, destination_port, ip_add, local_port, descr in protocols, dest_ports, ip_adds, local_ports, descrs:
+    #         pfsense.add_firewall_rule(protocol, destination_port, ip_add, descr)
+    #         pfsense.add_port_forward_rule(protocol, destination_port, ip_add, local_port, descr)
+    # pfsense.apply_changes()
 
 def update_port_forward_rule_ip_adds(vm_names, ip_adds):
-    lock = redis_client.lock('pfsense_lock', timeout=60)
-    with lock:
-        for vm_name, ip_add in vm_names, ip_adds:
-            port_forward_id = get_port_forward_rule(vm_name)
-            firewall_id = get_firewall_rule(vm_name)
-            pfsense.edit_port_forward_rule(port_forward_id, ip_add)
-            pfsense.edit_firewall_rule(firewall_id, ip_add)
+    return
+    # lock = redis_client.lock('pfsense_lock', timeout=60)
+    # with lock:
+    #     for vm_name, ip_add in vm_names, ip_adds:
+    #         port_forward_id = get_port_forward_rule(vm_name)
+    #         firewall_id = get_firewall_rule(vm_name)
+    #         pfsense.edit_port_forward_rule(port_forward_id, ip_add)
+    #         pfsense.edit_firewall_rule(firewall_id, ip_add)
 
 def delete_port_forward_rules(vm_names):
-    lock = redis_client.lock('pfsense_lock', timeout=60)
-    with lock:
-        for vm_name in vm_names:
-            port_forward_id = get_port_forward_rule(vm_name)
-            firewall_id = get_firewall_rule(vm_name)
-            pfsense.delete_port_forward_rule(port_forward_id)
-            pfsense.delete_firewall_rule(firewall_id)
+    return
+    # lock = redis_client.lock('pfsense_lock', timeout=60)
+    # with lock:
+    #     for vm_name in vm_names:
+    #         port_forward_id = get_port_forward_rule(vm_name)
+    #         firewall_id = get_firewall_rule(vm_name)
+    #         pfsense.delete_port_forward_rule(port_forward_id)
+    #         pfsense.delete_firewall_rule(firewall_id)
