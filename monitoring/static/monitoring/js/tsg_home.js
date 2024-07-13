a58f76fb05cce3549eed1c139ba9b9e1bfa181bc
patrick-ong <patrick_bennett_ong@dlsu.edu.ph>
2024-07-13 14:28:21 +0800

    .

--- monitoring/static/monitoring/js/tsg_home.js
+++ monitoring/static/monitoring/js/tsg_home.js
@@ -1,22 +1,49 @@
 $(document).ready(function () {
-    function init() {
-        $.ajax({
-            type: 'GET',
-            url: 'monitoring/aggregateData/',
-            data: {},
-            datatype: 'json',
-            success: function (response) {
-                drawLineGraph(response.coresResultList, '#cpu-chart', 'CPU Usage', 'cpu');
-                drawLineGraph(response.storageResultList, '#storage-chart', 'Storage Usage', 'storage');
-                drawMultiLineGraph(response.memoryFreeResultList, response.memoryUsedResultList, '#ram-chart', 'RAM Usage', 'memory_free', 'memory_used');
-                drawMultiLineGraph(response.networkInResultList, response.networkOutResultList, '#network-chart', 'Network Usage', 'network_in', 'network_out');
-            },
-            error: function (response) {
-                console.log(response);
-            }
-        });
-    }
+    function drawLineGraph(data, element, title, valueKey) {
+        var margin = { top: 20, right: 30, bottom: 30, left: 40 },
+            width = 460 - margin.left - margin.right,
+            height = 400 - margin.top - margin.bottom;
 
+        var svg = d3.select(element)
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        var x = d3.scaleTime()
+            .domain(d3.extent(data, function (d) { return new Date(d.time); }))
+            .range([0, width]);
+
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x));
+
+        var y = d3.scaleLinear()
+            .domain([0, d3.max(data, function (d) { return d[valueKey]; })])
+            .range([height, 0]);
+
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+        svg.append("path")
+            .datum(data)
+            .attr("fill", "none")
+            .attr("stroke", "steelblue")
+            .attr("stroke-width", 1.5)
+            .attr("d", d3.line()
+                .x(function (d) { return x(new Date(d.time)); })
+                .y(function (d) { return y(d[valueKey]); })
+            );
+
+        svg.append("text")
+            .attr("x", (width / 2))
+            .attr("y", 0 - (margin.top / 2))
+            .attr("text-anchor", "middle")
+            .style("font-size", "16px")
+            .style("text-decoration", "underline")
+            .text(title);
+    }
     function drawMultiLineGraph(data1, data2, element, title, valueKey1, valueKey2) {
         var margin = { top: 20, right: 30, bottom: 30, left: 40 },
             width = 460 - margin.left - margin.right,
@@ -100,6 +127,23 @@ $(document).ready(function () {
             .attr("x", 20)
             .attr("y", 30)
             .text("Used RAM");
+    }
+    function init() {
+        $.ajax({
+            type: 'GET',
+            url: 'monitoring/aggregateData/',
+            data: {},
+            datatype: 'json',
+            success: function (response) {
+                drawLineGraph(response.coresResultList, '#cpu-chart', 'CPU Usage', 'cpu');
+                drawLineGraph(response.storageResultList, '#storage-chart', 'Storage Usage', 'storage');
+                drawMultiLineGraph(response.memoryFreeResultList, response.memoryUsedResultList, '#ram-chart', 'RAM Usage', 'memory_free', 'memory_used');
+                drawMultiLineGraph(response.networkInResultList, response.networkOutResultList, '#network-chart', 'Network Usage', 'network_in', 'network_out');
+            },
+            error: function (response) {
+                console.log(response);
+            }
+        });
     }
 
     init();
