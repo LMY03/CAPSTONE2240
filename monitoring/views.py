09bc21435a00fd04eca5a0d0b4a38bb375a18789
hyisangie <angeline_shi@dlsu.edu.ph>
2024-07-16 10:55:27 +0800

    b

--- monitoring/views.py
+++ monitoring/views.py
@@ -32,7 +32,6 @@ def getData(request):
     
     #Get VM Info from Proxmox API
     vmids = proxmox.cluster.resources.get(type='vm')    
-    lxcids = proxmox.cluster.resources.get(type='lxc')
     VMList= []
     
     #Loop through each VM to get info
@@ -234,7 +233,7 @@ def getData(request):
         'totalMemoryResultList': totalMemoryResultList,
         'totalStorageUsedResultList': totalStorageUsedResultList,
         'vmList': VMList,
-        'vmids': lxcids
+        'vmids': vmids
     })
 
 
