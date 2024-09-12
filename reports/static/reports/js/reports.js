6fb0c14c4a779784080446efbf63d5a9200da306
hyisangie <angeline_shi@dlsu.edu.ph>
2024-09-12 15:17:30 +0800

    z

--- reports/static/reports/js/reports.js
+++ reports/static/reports/js/reports.js
@@ -1,45 +1,65 @@
 $(document).ready(function() {
 
   var vmids = []
+  var vmInfoTable;
 
-  // Initialize VM table in Report Page
-  var vmInfoTable = $('table#vmInfoTable').DataTable({
-    paging: true,
-    responsive: true,
-    autoWidth: false,
-    fixedColumns: true,
-    searching: true,
-    columnDefs: [
-      { orderable: false, "targets": 0 },
-      { targets: 0,
-        render: function(data, type, row, meta) {
-          return '<input type="checkbox" name="'+ row[1] + '" value="' + row[2] +'"/>'
+  // Listener for clicking the select all checkbox for all of the machines 
+  $('#cb-select-all').on('click', function() {
+    var rows = vmInfoTable.rows({'search': 'applied'}).nodes();
+    $('input[type="checkbox"]', rows).prop('checked', this.checked)
+  })
+
+  $('#vmInfoTable tbody').on('change', 'input[type="checkbox"]', function(){
+    if(!this.checked) {
+        var sel = $('#cb-select-all').get(0);
+        if(sel && sel.checked && ('indeterminate' in sel)) {
+            sel.indeterminate = true;
         }
-      }
-    ],
-    select: {
-      style: 'multi',
-    },
-    lengthMenu: [[10, 25, 50, -1], [10, 25, 50, "All"]],
-    order: [[1, 'asc']]
+    }
   })
 
   // Generate rows 
-  function getVmtable(vm) {
+  function genVmTable(vm) {
+    console.log(vm)
 
+    let vmInfo = [vmList.id, vmList.name, vmList.type, vmList.node]
+    vmInfoTable.row.add(vmInfo).draw();
   }
-
-
+  
   function init() {
     $.ajax({
       type: 'GET',
       url: 'getVmList',
       datatype: 'json',
       success: function(response) {
-        const vmList = JSON.parse(response.vmList);
-        vmList.forEach(function(val) {
-          genVmTable(val);
-        })
+
+        // Initializes the VM Table in the Reports Page
+        vmInfoTable = $('table#vmInfoTable').DataTable({
+          paging: true,
+          responsive: true,
+          autoWidth: false,
+          fixedColumns: true,
+          searching: true,
+          columnDefs :[
+              {   orderable: false, "targets": 0 },
+              {   targets: 0,
+                  render: function(data, type, row, meta){
+                      return '<input type="checkbox" name="'+ row[1] +'" value="'+ row[2] +'"/>'
+                  }
+              }
+          ],
+          select : {
+              style: 'multi',
+          },
+          lengthMenu: [[10, 25, 50, -1], [10, 25, 50, "All"]],
+          order: [[1, 'asc']]
+        });
+
+        // Generates the VM table for each item in the list of machines
+        for (i = 0; i < response.vmList.length; i++) {
+          genVmTable(response.vmList[i])
+        }
+
       },
       error: function(response) {
         console.log(response);
--- reports/views.py
+++ reports/views.py
@@ -13,11 +13,58 @@ def index(request):
 # Get VM Info
 def getVmList(request):
     
-    # Get All VMs
-    try:
-        vminfos = VirtualMachines.objects.all()
-        serialized_data = serializers.serialize('json', vminfos)
-        data = json.loads(serialized_data)
-        return JsonResponse({'vmList' : data}, safe=False)
-    except Exception as e:
-        return JsonResponse({'error': str(e)}, status=500)
+    proxmox = ProxmoxAPI('10.1.200.11', user='root@pam', password='cap2240', verify_ssl=False)
+    client = InfluxDBClient(url=INFLUX_ADDRESS, token=token, org=org)
+    
+    #Get VM Info from Proxmox API
+    vmids = proxmox.cluster.resources.get(type='vm')    
+    VMList= []
+    query_api = client.query_api()
+    #Query to get all nodes being used
+    
+    flux_query = f'''
+                    from(bucket:"{bucket}")
+                    |> range(start: -5m)
+                    |> filter(fn: (r) => r._measurement == "system")
+                    |> filter(fn: (r) => r.object == "nodes")
+                    |> group(columns: ["host"])
+                    |> distinct(column: "host")
+                    '''
+    
+    result = query_api.query(query=flux_query)
+    nodes = []
+    for table in result:
+        for record in table.records:
+            nodes.append(record.values.get("host", ""))
+
+    #Loop through each VM to get info
+    for vmid in vmids:
+        VMDict = {}
+        VMDict["id"] = vmid['vmid']
+        VMDict["type"] = vmid['type']        
+        VMDict["cpu"] = vmid['cpu']
+        VMDict["disk"] = vmid['disk']
+        VMDict["maxcpu"] = vmid['maxcpu']
+        VMDict["maxdisk"] = vmid['maxdisk']        
+        VMDict["maxmem"] = vmid['maxmem']
+        VMDict["mem"] = vmid['mem']
+        VMDict["name"] = vmid['name']
+        VMDict["node"] = vmid['node']
+        VMDict["status"] = vmid['status']
+        VMDict["uptime"] = vmid['uptime']
+        VMDict['network_in'] = vmid['netin']
+        VMDict['network_out'] = vmid['netout']
+        VMList.append(VMDict)
+
+    return JsonResponse({
+        'vmList': VMList,
+        'vmids': vmids,
+    })
+    # # Get All VMs - not able to get it
+    # try:
+    #     vminfos = VirtualMachines.objects.all()
+    #     serialized_data = serializers.serialize('json', vminfos)
+    #     data = json.loads(serialized_data)
+    #     return JsonResponse({'vmList' : data}, safe=False)
+    # except Exception as e:
+    #     return JsonResponse({'error': str(e)}, status=500)
