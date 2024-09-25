6bf9ae946840ee1bb054a61fadb38a08fbdc1e4f
hyisangie <angeline_shi@dlsu.edu.ph>
2024-09-26 00:26:53 +0800

    Update gen-reports.js

---
+++ reports/static/reports/js/gen-reports.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+
+  function init() {
+    $.ajax({
+      type: 'GET',
+      url: 'report_gen',
+      datatype: 'json',
+      success: function(response) {
+        console.log("Received Data" + response)
+      },
+      error: function(xhr) {
+        console.log(xhr.responseText)
+      }
+    })
+  }
+})
