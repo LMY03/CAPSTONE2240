abe4efde117031f8e26309d4cd9c8197d4a45ff3
hyisangie <angeline_shi@dlsu.edu.ph>
2024-09-26 01:20:49 +0800

    Update views.py

--- reports/views.py
+++ reports/views.py
@@ -343,7 +343,9 @@ def report_gen(request):
         
         # TODO: REMOVE!
         print(f"request: {request.session['formData']}")
-        
+        session_data = request.session['formData']
+        stat_list = session_data['statList']
+        print(f"stat_list: {stat_list}")
         metrics = [key for key in ['cpuUsage', 'memoryUsage', 'netin', 'netout'] if key in request.POST.get('statList')]
         selected_metrics = []
         for metric in metrics:
