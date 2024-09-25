f5fe6c17894a5b336a74ec6d4248ec60e826d8c4
hyisangie <angeline_shi@dlsu.edu.ph>
2024-09-26 01:14:42 +0800

    Update views.py

--- reports/views.py
+++ reports/views.py
@@ -341,10 +341,9 @@ def report_gen(request):
 
         # node_metrics = ['cpu', 'memused', 'netin', 'netout', 'memtotal', 'swaptotal']
         
-        # TODO: REMOVE!
-        print(f"request: {request.session['formData']}")
+        session_data = request.session['formData']
         
-        metrics = [key for key in ['cpuUsage', 'memoryUsage', 'netin', 'netout'] if key in request.POST.get('statList')]
+        metrics = [key for key in ['cpuUsage', 'memoryUsage', 'netin', 'netout'] if key in session_data.get('statList')]
         selected_metrics = []
         for metric in metrics:
             if metric == 'cpuUsage':
