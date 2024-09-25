ac165f4c33c40ba7572e15004d54605962e452e6
hyisangie <angeline_shi@dlsu.edu.ph>
2024-09-25 19:02:29 +0800

    Update urls.py

--- reports/urls.py
+++ reports/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('getVmList', views.getVmList, name="getVmList"),
     path('index_csv', views.index_csv, name="index_csv"),
-    path('open_report_page', views.open_report_page, name="open_report_page"),
+    # path('open_report_page', views.open_report_page, name="open_report_page"),
     path('report_gen', views.report_gen, name="report_gen")
 ]
