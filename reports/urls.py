f344e0339ea4b09c1bca34af767573ff008ff596
hyisangie <angeline_shi@dlsu.edu.ph>
2024-09-27 14:01:15 +0800

    Update urls.py

--- reports/urls.py
+++ reports/urls.py
@@ -6,7 +6,7 @@ app_name = "reports"
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('performance_gen', views.performance_gen, name="performance_gen")
+    path('performance_gen', views.performance_gen, name="performance_gen"),
     path('getVmList', views.getVmList, name="getVmList"),
     # path('index_csv', views.index_csv, name="index_csv"),
     # path('open_report_page', views.open_report_page, name="open_report_page"),
