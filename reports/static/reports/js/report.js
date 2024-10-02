44c0713c60fe7221e5358a774c294b1dcc93ee9c
hyisangie <angeline_shi@dlsu.edu.ph>
2024-10-02 21:43:26 +0800

    Update report.js

--- reports/static/reports/js/report.js
+++ reports/static/reports/js/report.js
@@ -2,4 +2,24 @@ $(document).ready(function () {
   $('.report-item').on('click', function() {
       $(this).find('.chevron').toggleClass('rotated');
   });
+
+  // Handle Form submission
+  $('#filterForm').on('submit', function(e) {
+    e.preventDefault();     // prevent the default action
+
+    $.ajax({
+      url: 'extract_general_stat',
+      type: "POST",
+      data: $(this).serialize(),
+      headers: {
+        "X-CSRFToken": $('input[name=csrfmiddlewaretoken]').val()
+      },
+      success: function(response) {
+
+      },
+      error: function(xhr, status, error) {
+
+      }
+    })
+  })
 });
