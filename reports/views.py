6bd65d6a5b899f4d568eb6c05024ae8b30a6c866
hyisangie <angeline_shi@dlsu.edu.ph>
2024-10-03 00:49:44 +0800

    Update views.py

--- reports/views.py
+++ reports/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from datetime import datetime
+from datetime import datetime, timedelta
 from django.http import JsonResponse, HttpResponse
 from django.core import serializers
 from proxmoxer import ProxmoxAPI
