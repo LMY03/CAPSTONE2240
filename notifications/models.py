b186bf945ddb7df2a9f40394e8e203a6cdb07273
patrick-ong <patrick_bennett_ong@dlsu.edu.ph>
2024-10-05 15:07:19 +0800

    commented out the models in notificatons app

--- notifications/models.py
+++ notifications/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 
 
-class Credentials(models.Model):
-    user_id = models.CharField(max_length=255)
-    access_token = models.CharField(max_length=255)
-    refresh_token = models.CharField(max_length=255)
-    expires_at = models.DateTimeField()  # Store when the token expires
-    created_at = models.DateTimeField(auto_now_add=True)  # When credentials were created
-    updated_at = models.DateTimeField(auto_now=True)  # When credentials were last updated
+# class Credentials(models.Model):
+#     user_id = models.CharField(max_length=255)
+#     access_token = models.CharField(max_length=255)
+#     refresh_token = models.CharField(max_length=255)
+#     expires_at = models.DateTimeField()  # Store when the token expires
+#     created_at = models.DateTimeField(auto_now_add=True)  # When credentials were created
+#     updated_at = models.DateTimeField(auto_now=True)  # When credentials were last updated
 
-    def __str__(self):
-        return f"{self.user.username} Credentials"
+#     def __str__(self):
+#         return f"{self.user.username} Credentials"
