3ba4e3fc270772e9033e80e88737b8a7ed4dd8b2
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-10-07 22:04:46 +0800

    Update views.py

--- proxmox/views.py
+++ proxmox/views.py
@@ -97,11 +97,11 @@ def shutdown_vm(vm : VirtualMachines):
         
         if not vm.is_lxc():
             
-            proxmox.shutdown_vm(vm.node.name, vm.vm_id)
+            proxmox.shutdown_lxc(vm.node.name, vm.vm_id)
 
             vm.set_shutdown()
 
-            proxmox.wait_for_vm_stop(vm.node.name, vm.vm_id)
+            proxmox.wait_for_lxc_stop(vm.node.name, vm.vm_id)
         else:
             
             proxmox.shutdown_vm(vm.node.name, vm.vm_id)
