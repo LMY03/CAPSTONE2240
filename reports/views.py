ebbb61e1ebcf6548a081c31138fa02e1b2fdecb6
hyisangie <angeline_shi@dlsu.edu.ph>
2024-10-13 15:49:05 +0800

    Update views.py

--- reports/views.py
+++ reports/views.py
@@ -1393,8 +1393,10 @@ def process_indiv_query_result(results, query_result, column_name):
                     break
 
 def get_time_window(start_datetime, end_datetime):
-    start = datetime.strptime(start_datetime, "%Y-%m-%d %H:%M:%S")
-    end = datetime.strptime(end_datetime, "%Y-%m-%d %H:%M:%S")
+    # start = datetime.strptime(start_datetime, "%Y-%m-%d %H:%M:%S")
+    # end = datetime.strptime(end_datetime, "%Y-%m-%d %H:%M:%S")
+    start = datetime.strptime(start_datetime, "%Y-%m-%d")
+    end = datetime.strptime(end_datetime, "%Y-%m-%d")
     
     start, end = min(start, end), max(start, end)
     
