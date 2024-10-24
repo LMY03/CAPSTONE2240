ab8915e2540f263c64b3144d9ab411b1bcf1c7c3
hyisangie <angeline_shi@dlsu.edu.ph>
2024-10-24 11:12:06 +0800

    change axis color

--- reports/static/reports/js/reports.js
+++ reports/static/reports/js/reports.js
@@ -265,12 +265,14 @@ function showchart(labels, datasets, title){
                                 font: {
                                     family: "'Ubuntu', sans-serif",
                                     size: 14
-                                }
+                                },
+                                color: '#ff6384'
                             },
                             ticks: {
                                 font: {
                                     family: "'Ubuntu', sans-serif"
-                                }
+                                },
+                                color: '#ff6384'
                             }
                         },
                         y1: {
@@ -283,12 +285,14 @@ function showchart(labels, datasets, title){
                                 font: {
                                     family: "'Ubuntu', sans-serif",
                                     size: 14
-                                }
+                                },
+                                color: '#36a2eb'
                             },
                             ticks: {
                                 font: {
                                     family: "'Ubuntu', sans-serif"
-                                }
+                                },
+                                color: '#36a2eb'
                             },
                             grid: {
                                 drawOnChartArea: false,
